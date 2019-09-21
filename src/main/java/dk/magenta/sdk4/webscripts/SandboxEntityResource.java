5a22da311cc41fc5c7a9f7e9684fd51f14f7ba28
Andreas Kring <andreas@magenta.dk>
2019-09-21 19:28:27 +0200

    Added multipart/form-data endpoint

--- src/main/java/dk/magenta/sdk4/webscripts/SandboxEntityResource.java
+++ src/main/java/dk/magenta/sdk4/webscripts/SandboxEntityResource.java
@@ -7,10 +7,14 @@ import org.alfresco.rest.framework.WebApiNoAuth;
 import org.alfresco.rest.framework.core.exceptions.EntityNotFoundException;
 import org.alfresco.rest.framework.resource.EntityResource;
 import org.alfresco.rest.framework.resource.actions.interfaces.EntityResourceAction;
+import org.alfresco.rest.framework.resource.actions.interfaces.MultiPartResourceAction;
 import org.alfresco.rest.framework.resource.parameters.Parameters;
+import org.alfresco.rest.framework.webscripts.WithResponse;
+import org.springframework.extensions.webscripts.servlet.FormData;
 
 @EntityResource(name = "sandbox", title = "Sandbox")
-public class SandboxEntityResource implements EntityResourceAction.ReadById<Node> {
+public class SandboxEntityResource implements EntityResourceAction.ReadById<Node>,
+        MultiPartResourceAction.Create<Node> {
 
     private Nodes nodes;
 
@@ -18,6 +22,13 @@ public class SandboxEntityResource implements EntityResourceAction.ReadById<Node
     @WebApiNoAuth
     public Node readById(String id, Parameters parameters) throws EntityNotFoundException {
         return AuthenticationUtil.runAsSystem(() -> nodes.getFolderOrDocument(id, parameters));
+    }
+
+    @Override
+    @WebApiNoAuth
+    public Node create(FormData formData, Parameters parameters, WithResponse withResponse) {
+        System.out.println("multipart");
+        return new Node();
     }
 
     public void setNodes(Nodes nodes) {
